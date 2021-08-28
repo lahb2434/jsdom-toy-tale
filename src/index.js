6e78faabfc20431026bc5cf41416c9769641971f
Christopher Houghton <ChrisHoughton@ChristopherMBP.fios-router.home>
2021-08-28 07:43:22 -0400

    commit tree 265e4d32cc43c39edfdb70ea2a6bf014ed52c8f1

--- src/index.js
+++ src/index.js
@@ -12,4 +12,56 @@ document.addEventListener("DOMContentLoaded", () => {
       toyFormContainer.style.display = "none";
     }
   });
+
+  fetch('http://localhost:3000/toys')
+    .then(response => response.json())
+    .then(json => json.forEach( toy => createToy(toy)));
+
+  const toyCollection = document.getElementById('toy-collection')
+  
+  createToy = (toy) => {
+      const div = document.createElement('div');
+        div.classList.add('card');
+      const h2 = document.createElement('h2');
+        h2.textContent = toy.name;
+      const img = document.createElement('img');
+        img.classList.add('toy-avatar');
+        img.src = toy.image;
+      const p = document.createElement('p');
+        p.textContent = toy.likes;
+      const button = document.createElement('button');
+        button.classList.add('like-btn')
+        button.textContent = 'like <3';
+      const toyAttributes = [h2, img, p, button];
+      toyAttributes.forEach( element => div.appendChild(element));
+      toyCollection.appendChild(div);
+  };
+
+  const addToyForm = document.querySelector('.add-toy-form')
+  
+  addToyForm.addEventListener('submit', (event) => {
+    event.preventDefault();
+    const form = event.currentTarget; 
+    const data = new FormData(form);
+    data.append('likes', 0)
+    form.reset();
+    const entries = Object.fromEntries(data);
+    const entriesToJson = JSON.stringify(entries);
+    addToyToDb(entriesToJson)
+      .then(response => response.json())
+      .then(toy => createToy(toy));
+    console.log(entriesToJson);
+  });
+
+  addToyToDb = (newToy) => {
+    return fetch('http://localhost:3000/toys', {
+	    method: 'POST',
+	    body: newToy,
+	    headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json"
+      }
+    });
+  };
+
 });
